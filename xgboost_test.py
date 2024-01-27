eaa1813df3d72c5d61a20e76ceb7f262dc6e04b7
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-01-27 02:21:10 -0500

    update

--- xgboost_test.py
+++ xgboost_test.py
@@ -18,67 +18,50 @@ y = data['E']
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-# loop the random state to get the best random state
-best_r2 = 0
-best_mse = 1000
-best_random_state = 0
 
-for i in range(1000):
-    print('Random state:', i)
-    # Splitting the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.1, random_state=i)
+# Splitting the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.1, random_state=539)
 
-    # Setting up the parameter grid for hyperparameter tuning
-    param_grid = {
-        'n_estimators': [100, 200, 300],
-        'learning_rate': [0.01, 0.05, 0.1],
-        'max_depth': [3, 4, 5],
-        'subsample': [0.8, 0.9, 1],
-        'colsample_bytree': [0.8, 0.9, 1],
-    }
+# Setting up the parameter grid for hyperparameter tuning
+param_grid = {
+    'n_estimators': [10, 50, 100, 200, 300, 400, 500],
+    'learning_rate': [0.01, 0.015, 0.02, 0.05],
+    'max_depth': [1, 2, 3, 4, 5],
+    'colsample_bytree': [0.6, 0.7, 0.8, 0.9, 1],
+}
 
-    # Creating the XGBRegressor model
-    xgb_model = XGBRegressor()
+# Creating the XGBRegressor model
+xgb_model = XGBRegressor()
 
-    # GridSearchCV for hyperparameter tuning
-    grid_search = GridSearchCV(estimator=xgb_model, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2, scoring='r2')
-    grid_search.fit(X_train, y_train)
+# GridSearchCV for hyperparameter tuning
+grid_search = GridSearchCV(estimator=xgb_model, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2, scoring='r2')
+grid_search.fit(X_train, y_train)
 
-    # Best parameters and best score
-    best_params = grid_search.best_params_
-    best_score = grid_search.best_score_
+# Best parameters and best score
+best_params = grid_search.best_params_
+best_score = grid_search.best_score_
 
-    # Training the model with the best parameters
-    best_xgb_model = XGBRegressor(**best_params)
-    best_xgb_model.fit(X_train, y_train)
+# Training the model with the best parameters
+best_xgb_model = XGBRegressor(**best_params)
+best_xgb_model.fit(X_train, y_train)
 
-    # Predicting and evaluating the model
-    y_pred = best_xgb_model.predict(X_test)
-    r2 = r2_score(y_test, y_pred)
-    mse = mean_squared_error(y_test, y_pred)
-
-    # Save the best random state
-    if r2 > best_r2:
-        best_r2 = r2
-        best_mse = mse
-        best_random_state = i
-
-print('Best random state:', best_random_state)
-print('Best R2 score:', best_r2)
-
+# Predicting and evaluating the model
+y_pred = best_xgb_model.predict(X_test)
+r2 = r2_score(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
 
 # Plot the parity plot
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# sns.set()
-#
-# plt.figure(figsize=(8, 8))
-# plt.scatter(y_test, y_pred)
-# plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], 'k--')
-# plt.xlabel('Actual')
-# plt.ylabel('Predicted')
-# plt.title('XGBoost Regressor')
-# plt.show()
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+plt.figure(figsize=(8, 8))
+plt.scatter(y_test, y_pred)
+plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], 'k--')
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+plt.title('XGBoost Regressor')
+plt.show()
 
 
 
