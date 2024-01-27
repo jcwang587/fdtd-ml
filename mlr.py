1e504f763c6e2c447d70ad880b1d63ad2db9d97b
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-01-26 23:01:33 -0500

    update

--- mlr.py
+++ mlr.py
@@ -3,7 +3,7 @@ from sklearn.metrics import mean_squared_error
 import pandas as pd
 
 # Load the data
-data = pd.read_csv('data.csv')
+data = pd.read_csv('ml-pillaring.csv')
 data_shuffled = data.sample(frac=1, random_state=587)
 
 # split the data to train, validation and test
@@ -12,13 +12,13 @@ val_idx = data_shuffled.index[int(0.8 * len(data_shuffled)):int(0.9 * len(data_s
 test_idx = data_shuffled.index[int(0.9 * len(data_shuffled)):]
 
 # split the data to train, validation and test, the first 5 columns are features, the last column is the target
-X_train = data_shuffled.iloc[train_idx, :-2]
+X_train = data_shuffled.iloc[train_idx, :-3]
 y_train = data_shuffled.iloc[train_idx, -1]
 
-X_val = data_shuffled.iloc[val_idx, :-2]
+X_val = data_shuffled.iloc[val_idx, :-3]
 y_val = data_shuffled.iloc[val_idx, -1]
 
-X_test = data_shuffled.iloc[test_idx, :-2]
+X_test = data_shuffled.iloc[test_idx, :-3]
 y_test = data_shuffled.iloc[test_idx, -1]
 
 # Create the model
--- xgboost_opt.py
+++ xgboost_opt.py
@@ -7,22 +7,22 @@ import warnings
 
 
 # Load the data
-data = pd.read_csv('data.csv')
+data = pd.read_csv('ml-pillaring.csv')
 data_shuffled = data.sample(frac=1, random_state=411)
 
 # split the data to train, validation and test
-train_idx = data_shuffled.index[:int(0.9 * len(data_shuffled))]
-valid_idx = data_shuffled.index[int(0.9 * len(data_shuffled)):]
+train_idx = data_shuffled.index[:int(0.8 * len(data_shuffled))]
+valid_idx = data_shuffled.index[int(0.8 * len(data_shuffled)):int(0.9 * len(data_shuffled))]
 test_idx = data_shuffled.index[int(0.9 * len(data_shuffled)):]
 
 # split the data to train, validation and test, the first 5 columns are features, the last column is the target
-X_train = data_shuffled.iloc[train_idx, :-2]
+X_train = data_shuffled.iloc[train_idx, :-3]
 y_train = data_shuffled.iloc[train_idx, -1]
 
-X_valid = data_shuffled.iloc[valid_idx, :-2]
+X_valid = data_shuffled.iloc[valid_idx, :-3]
 y_valid = data_shuffled.iloc[valid_idx, -1]
 
-X_test = data_shuffled.iloc[test_idx, :-2]
+X_test = data_shuffled.iloc[test_idx, :-3]
 y_test = data_shuffled.iloc[test_idx, -1]
 
 # Create the model
